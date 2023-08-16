8ddc1a7baf54281deaad87cc4340ea8ee05a6cae
ToukirMECBD <50370002+ToukirMECBD@users.noreply.github.com>
2023-08-16 19:40:07 +0600

    Create Symmetric_Tree.cpp

---
+++ week_05/Symmetric_Tree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    private:
+    bool isMirror(TreeNode* p,TreeNode* q) {
+        if(p==nullptr && q==nullptr) return true;
+        if(p==nullptr || q==nullptr) return false;
+        return (p->val==q->val) &&
+                isMirror(p->left,q->right) &&
+                isMirror(p->right,q->left);
+    }
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(root==nullptr) return true;
+        return isMirror(root->left,root->right);
+    }
+};
