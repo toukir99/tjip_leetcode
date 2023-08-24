8bbbe70178fde1a071d538639348d8f90423dfbe
ToukirMECBD <50370002+ToukirMECBD@users.noreply.github.com>
2023-08-25 00:41:43 +0600

    Create 4SumI_II.cpp

---
+++ week_04/4SumI_II.cpp
@@ -0,0 +1,20 @@
+//tc: O(n^2)
+//sc: O(n^2)
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        unordered_map<int,int>pair_sum;
+        for(auto x: nums1){
+            for(auto y:nums2){
+                pair_sum[x+y]++;
+            }
+        }
+        int tuple_cnt=0;
+        for(auto x: nums3){
+            for(auto y:nums4){
+                tuple_cnt+=pair_sum[-(x+y)];
+            }
+        }
+        return tuple_cnt;
+    }
+};
