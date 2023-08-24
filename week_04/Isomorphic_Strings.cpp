84b66da9a5b622e44d8783f35638a97b0225c524
ToukirMECBD <50370002+ToukirMECBD@users.noreply.github.com>
2023-08-25 00:26:37 +0600

    Create Isomorphic_Strings.cpp

---
+++ week_04/Isomorphic_Strings.cpp
@@ -0,0 +1,20 @@
+//tc: O(n)
+//sc: O(n)
+
+class Solution {
+public:
+	bool isIsomorphicOneWay(string &s, string &t) {
+        unordered_map<char,char>from;
+			 for(int i=0;i<s.size();i++){
+				 if(from.count(s[i]) and from[s[i]]!=t[i])
+				 {
+					 return false;
+				 }
+				 from[s[i]]=t[i];
+			 }
+			 return true;
+    }
+    bool isIsomorphic(string s, string t) {
+       return isIsomorphicOneWay(s,t) and isIsomorphicOneWay(t,s);
+    }
+};
